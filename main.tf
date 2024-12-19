a53acb426e1bae63a31c5b71028170ab225fb82a
Rogatiq <aleksandarivanov@Aleksandars-MacBook-Pro-M3.local>
2024-12-19 21:32:27 +0100

    update

--- main.tf
+++ main.tf
@@ -205,6 +205,28 @@ resource "aws_launch_template" "web_lt" {
     }
   }
 }
+resource "aws_autoscaling_group" "web_asg" {
+  name             = "${var.prefix}-web-asg"
+  desired_capacity = 2
+  max_size         = 3
+  min_size         = 2
+  launch_template {
+    id      = aws_launch_template.web_lt.id
+    version = "$Latest"
+  }
+
+  # Use both public subnets here
+  vpc_zone_identifier = [
+    aws_subnet.public_subnet_1.id,
+    aws_subnet.public_subnet_2.id
+  ]
+
+  target_group_arns = [aws_lb_target_group.web_tg.arn]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
 # 4. DynamoDB creation
 resource "aws_dynamodb_table" "filenames_table" {
@@ -359,3 +381,42 @@ resource "aws_lambda_permission" "allow_s3" {
   source_arn    = aws_s3_bucket.files_bucket.arn
 }
 
+
+### 8. Load Balancer
+resource "aws_lb" "alb" {
+  name               = "${var.prefix}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb_sg.id]
+  subnets = [
+    aws_subnet.public_subnet_1.id,
+    aws_subnet.public_subnet_2.id
+  ]
+
+  tags = {
+    Name = "${var.prefix}-alb"
+  }
+}
+
+resource "aws_lb_target_group" "web_tg" {
+  name     = "${var.prefix}-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+}
+
+resource "aws_lb_listener" "web_listener" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web_tg.arn
+  }
+}
+
+### 9. Outputs
+output "alb_dns_name" {
+  value = aws_lb.alb.dns_name
+}
